814a768e27b6809fe14283b9dca8a30a79afd878
suxarik <suxarik312004gor@gmail.com>
2023-12-20 10:06:08 +0200

    setup new project

---
+++ src/test/java/com/example/lab23/Lab23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
