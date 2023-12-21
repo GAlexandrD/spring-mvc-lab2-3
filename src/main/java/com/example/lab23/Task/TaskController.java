42094aa3676e83a24315488976f4dbebf30f6436
Hatrison <vadimulanovskiy@gmail.com>
2023-12-21 12:53:05 +0200

    Added updating tasks

--- src/main/java/com/example/lab23/Task/TaskController.java
+++ src/main/java/com/example/lab23/Task/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.lab23.Task;
 
 import com.example.lab23.Task.dto.CreateTaskDto;
+import com.example.lab23.Task.dto.UpdateTaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,6 +46,34 @@ public class TaskController {
   @GetMapping("/create-task")
   public String createTask(Model model) {
     return "create-task";
+  }
+
+  @GetMapping("/task-updated")
+  public String updateTask(Model model,
+      @RequestParam(name = "id") Long taskId,
+      @RequestParam(name = "taskName") String taskName,
+      @RequestParam(name = "deadline") String deadline,
+      @RequestParam(name = "priority", required = false) Integer priority) {
+    if (taskName.isEmpty())
+      taskName = null;
+    if (deadline.isEmpty())
+      deadline = null;
+    taskService.updateTask(taskId, new UpdateTaskDto(taskName, deadline, priority, null));
+    model.addAttribute("tasks", taskService.getAllTasks());
+    return "task-list";
+  }
+
+  @GetMapping("/mark-done")
+  public String markDone(Model model, @RequestParam(name = "taskId") Long id) {
+    taskService.updateTask(id, new UpdateTaskDto(null, null, null, true));
+    model.addAttribute("tasks", taskService.getAllTasks());
+    return "task-list";
+  }
+
+  @GetMapping("/update-task")
+  public String updateTask(Model model, @RequestParam("taskId") Long taskId) {
+    model.addAttribute("task", taskService.getTaskById(taskId));
+    return "update-task";
   }
 
   @GetMapping("/delete")
